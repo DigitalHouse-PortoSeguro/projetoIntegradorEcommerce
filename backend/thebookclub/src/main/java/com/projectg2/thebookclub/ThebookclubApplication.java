07a8afca8307c9f904a02ba4a1be5d7232a7e6a1
Adriana Mucciolo <drishaolin@gmail.com>
2022-04-24 20:46:20 -0300

    estrutura inicial do projeto com spring initializr

---
+++ backend/thebookclub/src/main/java/com/projectg2/thebookclub/ThebookclubApplication.java
@@ -0,0 +1,13 @@
+package com.projectg2.thebookclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThebookclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThebookclubApplication.class, args);
+	}
+
+}
---
+++ backend/thebookclub/src/test/java/com/projectg2/thebookclub/ThebookclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectg2.thebookclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThebookclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
