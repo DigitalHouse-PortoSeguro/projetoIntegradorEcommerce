a86aadb1e553911979c1a88b2b63e4e07ad8c320
ghsoares <ghsoares99795@gmail.com>
2022-05-17 19:08:16 -0300

    Adicionado método 'findByUsuario'

--- backend/thebookclub/src/main/java/br/com/digitalhouse/thebookclub/repository/UsuarioRepository.java
+++ backend/thebookclub/src/main/java/br/com/digitalhouse/thebookclub/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package br.com.digitalhouse.thebookclub.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,6 @@ import br.com.digitalhouse.thebookclub.modelo.Usuario;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
+	public Optional<Usuario> findByUsuario(String usuario);
 	
 }
