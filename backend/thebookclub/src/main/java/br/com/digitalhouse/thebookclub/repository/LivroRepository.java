d4f59e8e7b2c09b9c42c44bece5b958cd29b17c2
Rui Temgi <ruisaraivasp@gmail.com>
2022-06-02 12:14:11 -0300

    Adicionado o Método de Pesquisa por Título no Repository

--- backend/thebookclub/src/main/java/br/com/digitalhouse/thebookclub/repository/LivroRepository.java
+++ backend/thebookclub/src/main/java/br/com/digitalhouse/thebookclub/repository/LivroRepository.java
@@ -1,12 +1,14 @@
 package br.com.digitalhouse.thebookclub.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import br.com.digitalhouse.thebookclub.modelo.Livro;
 
+import br.com.digitalhouse.thebookclub.modelo.Livro;
 
 @Repository
 public interface LivroRepository extends JpaRepository<Livro, Long> {
-
 	
+	public List<Livro> findAllByTituloContainingIgnoreCase(String titulo);
 }
