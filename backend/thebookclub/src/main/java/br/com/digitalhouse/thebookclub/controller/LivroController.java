3dfc6a2ad9b24de94a67faf91b76d577ad0aa81f
Rui Temgi <ruisaraivasp@gmail.com>
2022-06-02 12:03:56 -0300

    Adicionada a Classe LivroController com Endpoint Consultar

---
+++ backend/thebookclub/src/main/java/br/com/digitalhouse/thebookclub/controller/LivroController.java
@@ -0,0 +1,40 @@
+package br.com.digitalhouse.thebookclub.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.digitalhouse.thebookclub.modelo.Livro;
+import br.com.digitalhouse.thebookclub.repository.LivroRepository;
+
+@RestController
+@RequestMapping("/livros")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class LivroController {
+	
+	@Autowired
+	private LivroRepository livroRepository;
+	
+	@GetMapping("/consultar")
+	public ResponseEntity<List<Livro>> GetAll(){
+		return ResponseEntity.ok(livroRepository.findAll());
+	}
+	
+	@GetMapping("/consultar/{id}")
+	public ResponseEntity<Livro> GetById(@PathVariable Long id){
+		return livroRepository.findById(id)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
+}
