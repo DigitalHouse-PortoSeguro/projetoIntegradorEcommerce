07cbac60977dd696e6ea5d957d11a0d6f5b58b10
Rui Temgi <ruisaraivasp@gmail.com>
2022-06-02 13:33:20 -0300

    Refatorando StatusPedido com Construtor private e Método de Acesso Externo Static

--- backend/thebookclub/src/main/java/br/com/digitalhouse/thebookclub/enums/StatusPedido.java
+++ backend/thebookclub/src/main/java/br/com/digitalhouse/thebookclub/enums/StatusPedido.java
@@ -3,19 +3,28 @@ package br.com.digitalhouse.thebookclub.enums;
 public enum StatusPedido {
 	
 	AGUARDANDO_PAGAMENTO(1),
-	PROCESSANDO(2),
-	PAGTO_APROVADO(3),
-	PAGTO_RECUSADO(4),
-	ENVIADO(5),
-	ENTREGUE(6);
+	PAGO(2),
+	PROCESSANDO(3),
+	DESPACHADO(4),
+	ENTREGUE(5),
+	CANCELADO(6); 
 	
 	private int valor;
 	
-	StatusPedido(int valor){
+	private StatusPedido(int valor){
 		this.valor = valor;
 	}
 	
 	public int getValor() {
 		return valor;
+	}
+	
+	public static StatusPedido valueOf(int valor){
+		for(StatusPedido value : StatusPedido.values() ){
+			if(valor == value.getValor()){
+				return value;
+			}
+		}
+		throw new IllegalArgumentException("Status do Pedido Inválido");
 	}
 }
