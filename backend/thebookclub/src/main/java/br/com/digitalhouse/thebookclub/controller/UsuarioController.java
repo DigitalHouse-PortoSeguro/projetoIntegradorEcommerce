fb789c22b99a66b6aa904142a9e49e4067636bca
Rui Temgi <ruisaraivasp@gmail.com>
2022-06-02 13:19:24 -0300

    Classe Refatorada Adicionado o Endpoint de Consulta Inicial

--- backend/thebookclub/src/main/java/br/com/digitalhouse/thebookclub/controller/UsuarioController.java
+++ backend/thebookclub/src/main/java/br/com/digitalhouse/thebookclub/controller/UsuarioController.java
@@ -1,31 +1,37 @@
 package br.com.digitalhouse.thebookclub.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.digitalhouse.thebookclub.modelo.Usuario;
-import br.com.digitalhouse.thebookclub.service.UsuarioService;
+import br.com.digitalhouse.thebookclub.repository.UsuarioRepository;
 
 @RestController
 @RequestMapping("/usuarios")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UsuarioController {
+	
 	@Autowired
-	public UsuarioService usuarioService;
-
-	@PostMapping("/cadastrar")
-	public ResponseEntity<Usuario> post(@RequestBody Usuario usuario) {
-		//@formatter:off
-		return ResponseEntity.status(HttpStatus.CREATED)
-				.body(usuarioService.cadastrarUsuario(usuario));
+	private UsuarioRepository usuarioRepository;
+	
+	@GetMapping("/consultar")
+	public ResponseEntity<List<Usuario>> GetAll(){
+		return ResponseEntity.ok(usuarioRepository.findAll());
 	}
 	
-	//TODO criar metodo para a requisição proveniente de "/logar"
-
+	
+	
+	
 }
