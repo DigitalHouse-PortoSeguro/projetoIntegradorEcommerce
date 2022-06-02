cd65795fc0d025d398f7b7025bf3f7c677427ffd
Rui Temgi <ruisaraivasp@gmail.com>
2022-06-02 12:18:34 -0300

    Adicionado Novas Anotações para Validação de Campos

--- backend/thebookclub/src/main/java/br/com/digitalhouse/thebookclub/modelo/Pedido.java
+++ backend/thebookclub/src/main/java/br/com/digitalhouse/thebookclub/modelo/Pedido.java
@@ -28,97 +28,122 @@ public class Pedido {
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Long pedidoId;
+	private Long idPedido;
 	
-	@NotNull
-	@Size(min=2,max=10)
-	private Double valor;
 	
-	@NotNull
-	@Enumerated(EnumType.STRING)
-	private TipoPagamento pagamento;
+	@NotNull(message="Este Campo é de Preenchimento Obrigatório e Não Pode Ser Vazio")
+	@Size(min=2,max=100)
+	private String usuarioPedido;
 	
-	@NotNull
-	@Enumerated(EnumType.STRING)
-	private FormaEnvio formaEnvio;
+	@NotNull(message="Este Campo é de Preenchimento Obrigatório e Não Pode Ser Vazio")
+	@Digits(integer=3, fraction=2, message="Preço Possui 3 Casas Inteiras e 2 Casas Após o Ponto")
+	private BigDecimal valor;
 	
-	@NotNull
+	@NotNull(message="Este Campo é de Preenchimento Obrigatório e Não Pode Ser Vazio")
 	@Enumerated(EnumType.STRING)
 	private StatusPedido status;
 	
-	@NotNull
-	@Temporal(TemporalType.DATE)
-	private Date dataPedido;
+	@NotNull(message="Este Campo é de Preenchimento Obrigatório e Não Pode Ser Vazio")
+	@Enumerated(EnumType.STRING)
+	private FormaEnvio formaEnvio;
 	
-	@NotNull
-	@Temporal(TemporalType.DATE)
-	private Date dataEntraga;
+	@NotNull(message="Este Campo é de Preenchimento Obrigatório e Não Pode Ser Vazio")
+	@Enumerated(EnumType.STRING)
+	private TipoPagamento tipoPagamento;
+	
+	@NotNull(message="Este Campo é de Preenchimento Obrigatório e Não Pode Ser Vazio")
+	@Pattern(regexp = "\\d{2}/\\d{2}/\\d{4}", message="DATA Deve ser Preenchido no Formato dd/MM/YYYY")
+	private String dataPedido;
+	
+	@NotNull(message="Este Campo é de Preenchimento Obrigatório e Não Pode Ser Vazio")
+	@Pattern(regexp = "\\d{2}/\\d{2}/\\d{4}", message="DATA Deve ser Preenchido no Formato dd/MM/YYYY")
+	private String dataEntrega;
+	
+	
 	
 	@OneToMany(mappedBy = "pedido", cascade = CascadeType.REMOVE)
 	@JsonIgnoreProperties("tb_pedido")
 	private List<PedidoLivro> livros;
 
-	public Long getPedidoId() {
-		return pedidoId;
+	
+	
+	public Long getIdPedido() {
+		return idPedido;
 	}
 
-	public Double getValor() {
+	public void setIdPedido(Long idPedido) {
+		this.idPedido = idPedido;
+	}
+
+	public List<PedidoLivro> getPedidoLivro_fk() {
+		return pedidoLivro_fk;
+	}
+
+	public void setPedidoLivro_fk(List<PedidoLivro> pedidoLivro_fk) {
+		this.pedidoLivro_fk = pedidoLivro_fk;
+	}
+
+	public Usuario getUsuario_fk() {
+		return usuario_fk;
+	}
+
+	public void setUsuario_fk(Usuario usuario_fk) {
+		this.usuario_fk = usuario_fk;
+	}
+
+	public String getUsuarioPedido() {
+		return usuarioPedido;
+	}
+
+	public void setUsuarioPedido(String usuarioPedido) {
+		this.usuarioPedido = usuarioPedido;
+	}
+
+	public BigDecimal getValor() {
 		return valor;
 	}
 
-	public TipoPagamento getPagamento() {
-		return pagamento;
-	}
-
-	public FormaEnvio getFormaEnvio() {
-		return formaEnvio;
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
 	}
 
 	public StatusPedido getStatus() {
 		return status;
 	}
 
-	public Date getDataPedido() {
-		return dataPedido;
+	public void setStatus(StatusPedido status) {
+		this.status = status;
 	}
 
-	public Date getDataEntraga() {
-		return dataEntraga;
-	}
-
-	public List<PedidoLivro> getLivros() {
-		return livros;
-	}
-
-	public void setPedidoId(Long pedidoId) {
-		this.pedidoId = pedidoId;
-	}
-
-	public void setValor(Double valor) {
-		this.valor = valor;
-	}
-
-	public void setPagamento(TipoPagamento pagamento) {
-		this.pagamento = pagamento;
+	public FormaEnvio getFormaEnvio() {
+		return formaEnvio;
 	}
 
 	public void setFormaEnvio(FormaEnvio formaEnvio) {
 		this.formaEnvio = formaEnvio;
 	}
 
-	public void setStatus(StatusPedido status) {
-		this.status = status;
+	public TipoPagamento getTipoPagamento() {
+		return tipoPagamento;
 	}
 
-	public void setDataPedido(Date dataPedido) {
+	public void setTipoPagamento(TipoPagamento tipoPagamento) {
+		this.tipoPagamento = tipoPagamento;
+	}
+
+	public String getDataPedido() {
+		return dataPedido;
+	}
+
+	public void setDataPedido(String dataPedido) {
 		this.dataPedido = dataPedido;
 	}
 
-	public void setDataEntraga(Date dataEntraga) {
-		this.dataEntraga = dataEntraga;
+	public String getDataEntrega() {
+		return dataEntrega;
 	}
 
-	public void setLivros(List<PedidoLivro> livros) {
-		this.livros = livros;
+	public void setDataEntrega(String dataEntrega) {
+		this.dataEntrega = dataEntrega;
 	}
 }
