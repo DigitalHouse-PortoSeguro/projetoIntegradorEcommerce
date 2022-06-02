98784cf089cac549c1ca2109937737e7a7d52eb9
Rui Temgi <ruisaraivasp@gmail.com>
2022-06-02 12:36:27 -0300

    Adicionado o Método de Pesquisa por Usuario no Repository

--- backend/thebookclub/src/main/java/br/com/digitalhouse/thebookclub/repository/PedidoRepository.java
+++ backend/thebookclub/src/main/java/br/com/digitalhouse/thebookclub/repository/PedidoRepository.java
@@ -1,5 +1,7 @@
 package br.com.digitalhouse.thebookclub.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import br.com.digitalhouse.thebookclub.modelo.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-
+	
+	public List<Pedido> findAllByUsuarioPedidoContainingIgnoreCase(String usuarioPedido);
 }
