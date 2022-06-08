406222a665e63ab2f2eb581d8cd4655dd5815e37
ghsoares <ghsoares99795@gmail.com>
2022-06-08 18:01:33 -0300

    Re-generated getters and setters

--- backend/thebookclub/src/main/java/br/com/digitalhouse/thebookclub/modelo/Usuario.java
+++ backend/thebookclub/src/main/java/br/com/digitalhouse/thebookclub/modelo/Usuario.java
@@ -97,94 +97,128 @@ public class Usuario {
 	public Long getId_Usuario() {
 		return id_Usuario;
 	}
-	public void setId_Usuario(Long id_Usuario) {
-		this.id_Usuario = id_Usuario;
-	}
-	public String getCpf() {
-		return cpf;
-	}
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
+
 	public String getNome() {
 		return nome;
 	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+
 	public String getSobrenome() {
 		return sobrenome;
 	}
-	public void setSobrenome(String sobrenome) {
-		this.sobrenome = sobrenome;
+
+	public String getCpf() {
+		return cpf;
 	}
+
 	public String getUsername() {
 		return username;
 	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
+
 	public String getTipoUsuario() {
 		return tipoUsuario;
 	}
-	public void setTipoUsuario(String tipoUsuario) {
-		this.tipoUsuario = tipoUsuario;
-	}
+
 	public String getEmail() {
 		return email;
 	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
+
 	public String getSenha() {
 		return senha;
 	}
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
+
 	public Date getDataNascimento() {
 		return dataNascimento;
 	}
-	public void setDataNascimento(Date dataNascimento) {
-		this.dataNascimento = dataNascimento;
-	}
+
 	public String getPreferencias() {
 		return preferencias;
 	}
-	public void setPreferencias(String preferencias) {
-		this.preferencias = preferencias;
-	}
+
 	public String getRua() {
 		return rua;
 	}
-	public void setRua(String rua) {
-		this.rua = rua;
-	}
+
 	public Integer getNumero() {
 		return numero;
 	}
-	public void setNumero(Integer numero) {
-		this.numero = numero;
-	}
+
 	public String getBairro() {
 		return bairro;
 	}
-	public void setBairro(String bairro) {
-		this.bairro = bairro;
-	}
+
 	public String getCep() {
 		return cep;
 	}
-	public void setCep(String cep) {
-		this.cep = cep;
-	}
+
 	public String getComplemento() {
 		return complemento;
 	}
+
+	public List<Pedido> getPedido() {
+		return pedido;
+	}
+
+	public void setId_Usuario(Long id_Usuario) {
+		this.id_Usuario = id_Usuario;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setTipoUsuario(String tipoUsuario) {
+		this.tipoUsuario = tipoUsuario;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public void setPreferencias(String preferencias) {
+		this.preferencias = preferencias;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
 	public void setComplemento(String complemento) {
 		this.complemento = complemento;
 	}
-	public List<Pedido> getPedido() {
-		return pedido;
+
+	public void setPedido(List<Pedido> pedido) {
+		this.pedido = pedido;
 	}
 }
