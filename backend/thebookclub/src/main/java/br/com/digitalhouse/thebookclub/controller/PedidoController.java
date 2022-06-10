0ace07b8088f4f08343015835d832752478c28ba
ghsoares <ghsoares99795@gmail.com>
2022-06-10 14:53:46 -0300

    revision of Pedido

--- backend/thebookclub/src/main/java/br/com/digitalhouse/thebookclub/controller/PedidoController.java
+++ backend/thebookclub/src/main/java/br/com/digitalhouse/thebookclub/controller/PedidoController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.digitalhouse.thebookclub.enums.FormaEnvio;
+import br.com.digitalhouse.thebookclub.enums.TipoPagamento;
 import br.com.digitalhouse.thebookclub.modelo.Pedido;
 import br.com.digitalhouse.thebookclub.modelo.PedidoLivro;
 import br.com.digitalhouse.thebookclub.repository.PedidoRepository;
@@ -21,55 +23,50 @@ import io.swagger.v3.oas.annotations.parameters.RequestBody;
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PedidoController {
 
-    //Linkando com o repository
-    @Autowired
-    private PedidoRepository repository;
+	// Linkando com o repository
+	@Autowired
+	private PedidoRepository repository;
 
-    //Retorna a lista de pedidos
-    @GetMapping
-    public ResponseEntity<List<Pedido>>getAll()
-    {
-        return ResponseEntity.ok(repository.findAll());
-    }
+	// Retorna a lista de pedidos
+	@GetMapping
+	public ResponseEntity<List<Pedido>> getAll() {
+		return ResponseEntity.ok(repository.findAll());
+	}
 
-    //Retorna pedido por id
-    @GetMapping("pedido/{pedidoId}")
-    public ResponseEntity<Pedido> GetById(@PathVariable long pedidoId) {
-        return repository.findById(pedidoId)
-        .map(resp -> ResponseEntity.ok(resp))
-        .orElse((ResponseEntity.notFound().build()));
-    }
+	// Retorna pedido por id
+	@GetMapping("pedido/{pedidoId}")
+	public ResponseEntity<Pedido> getById(@PathVariable long pedidoId) {
+		return repository.findById(pedidoId).map(resp -> ResponseEntity.ok(resp))
+				.orElse((ResponseEntity.notFound().build()));
+	}
 
-    // //Retorna pedido por valor
-    // @GetMapping("pedido/{valor}")
-    // public ResponseEntity<Pedido> GetById(@PathVariable double valor) {
-    //     return repository.findById(valor)
-    //     .map(resp -> ResponseEntity.ok(resp))
-    //     .orElse((ResponseEntity.notFound().build()));
-    // }
+	// //Retorna pedido por valor
+	// @GetMapping("pedido/{valor}")
+	// public ResponseEntity<Pedido> GetById(@PathVariable double valor) {
+	// return repository.findById(valor)
+	// .map(resp -> ResponseEntity.ok(resp))
+	// .orElse((ResponseEntity.notFound().build()));
+	// }
 
-    
-      //Retorna pedido por meio de pagamento
-      @GetMapping("pedido/{pagamento}")
-      public ResponseEntity<Pedido> GetById(@PathVariable TipoPagamento pagamento) {
-          return repository.findByTipoPagamento(pagamento)
-          .map(resp -> ResponseEntity.ok(resp))
-          .orElse((ResponseEntity.notFound()));
-      }
+	// Retorna pedido por meio de pagamento
+	@GetMapping("pedido/{pagamento}")
+	public ResponseEntity<Pedido> getByTipoPagamento(@PathVariable TipoPagamento pagamento) {
+		return repository.findByTipoPagamento(pagamento).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
 
-      
-    //Retorna os pedidos que contem o parametro 'formaEnvio'
-    @GetMapping("/pedido/{formaEnvio}")
-    public ResponseEntity<Pedido> GetByLFormaEnvio(@PathVariable FormaEnvio formaEnvio)
-    {
-        return ResponseEntity.ok(repository.findAllByFormaEnvioContainingIgnoreCase(formaEnvio));
-    }
+	// Retorna os pedidos que contem o parametro 'formaEnvio'
+	@GetMapping("/pedido/{formaEnvio}")
+	public ResponseEntity<Pedido> getByFormaEnvio(@PathVariable FormaEnvio formaEnvio) {
+		return repository.findByFormaEnvio(formaEnvio).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
 
-    //Retorna os pedidos que contem o parametro 'livros'
-    @GetMapping("/livros/{livros}")
-    public ResponseEntity<Pedido> GetByLivros(@PathVariable PedidoLivro livros)
-    {
-        return ResponseEntity.ok(repository.findAllByLivrosContainingIgnoreCase(livros));
-    }
+	// Retorna os pedidos que contem o parametro 'livros'
+	// @GetMapping("/livros/{livros}")
+	// public ResponseEntity<Pedido> getByLivros(@PathVariable PedidoLivro livros) {
+	//	return repository.findAllByLivrosContainingIgnoreCase(livros).map(resp -> ResponseEntity.ok(resp))
+	//			.orElse(ResponseEntity.notFound().build());
+	// }
 
 }
--- backend/thebookclub/src/main/java/br/com/digitalhouse/thebookclub/repository/PedidoRepository.java
+++ backend/thebookclub/src/main/java/br/com/digitalhouse/thebookclub/repository/PedidoRepository.java
@@ -1,20 +1,25 @@
 package br.com.digitalhouse.thebookclub.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import br.com.digitalhouse.thebookclub.enums.FormaEnvio;
+import br.com.digitalhouse.thebookclub.enums.TipoPagamento;
 import br.com.digitalhouse.thebookclub.modelo.Pedido;
 import br.com.digitalhouse.thebookclub.modelo.PedidoLivro;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
-    public findAllByLivrosContainingIgnoreCase(PedidoLivro livros);
+    // public List<Pedido> findAllByLivrosContainingIgnoreCase(PedidoLivro livros);
 
-    public findByPedidoId(long pedidoId);
+    public Optional<Pedido> findByPedidoId(long pedidoId);
 
-    public findByTipoPagamento(TipoPagamento pagamento);
+    public Optional<Pedido> findByTipoPagamento(TipoPagamento pagamento);
 
-    public GetByLFormaEnvio(FormaEnvio formaEnvio);
+    public Optional<Pedido> findByFormaEnvio(FormaEnvio formaEnvio);
     
 }
