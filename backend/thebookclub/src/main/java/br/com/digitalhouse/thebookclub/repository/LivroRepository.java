be672762d0e991492003c46b7dfd21f5f671b32a
ghsoares <ghsoares99795@gmail.com>
2022-06-12 17:18:54 -0300

    Alterações no LivroRepository

--- backend/thebookclub/src/main/java/br/com/digitalhouse/thebookclub/repository/LivroRepository.java
+++ backend/thebookclub/src/main/java/br/com/digitalhouse/thebookclub/repository/LivroRepository.java
@@ -1,6 +1,7 @@
 package br.com.digitalhouse.thebookclub.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,4 +12,5 @@ import br.com.digitalhouse.thebookclub.modelo.Livro;
 public interface LivroRepository extends JpaRepository<Livro, Long> {
 	
 	public List<Livro> findAllByTituloContainingIgnoreCase(String titulo);
+	public Optional<Livro> findByIsbn(String isbn);
 }
