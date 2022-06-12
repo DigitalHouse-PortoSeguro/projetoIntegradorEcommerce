e3357f417d32b4893a314eaea53559d69a652cf2
Adriana Mucciolo <drishaolin@gmail.com>
2022-06-12 19:00:15 -0300

    Componente input-custom e início da criação do formulario de cadastro do usuário

--- frontend/thebookclub-angular/src/app/app.module.ts
+++ frontend/thebookclub-angular/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UsuarioCadastroComponent } from './components/usuario-cadastro/usuario-cadastro.component';
+import { InputCustomComponent } from './components/input-custom/input-custom.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsuarioCadastroComponent,
+    InputCustomComponent
   ],
   imports: [
     BrowserModule
---
+++ frontend/thebookclub-angular/src/app/components/input-custom/input-custom.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-custom',
+  templateUrl: './input-custom.component.html',
+  styleUrls: ['./input-custom.component.css']
+})
+export class InputCustomComponent implements OnInit {
+
+  @Input() label: string;
+  @Input() type:string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
