2fb0c1b852b50418c31c93410b367ab15df26dbd
Adrielle Barros <adriellebarros31@gmail.com>
2022-06-13 22:21:03 -0300

    criando a nova branch nav-rodape e os componentes navbar e rodape

--- frontend/thebookclub-angular/src/app/app.module.ts
+++ frontend/thebookclub-angular/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RodapeComponent } from './components/rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule
