7e72968abe2aa790988fe1af85728a8afa1a044f
ghsoares <ghsoares99795@gmail.com>
2022-06-19 17:14:25 -0300

    Teste do card livro

--- frontend/thebookclub-angular/src/app/app.component.ts
+++ frontend/thebookclub-angular/src/app/app.component.ts
@@ -1,11 +1,5 @@
 import { Component } from '@angular/core';
-import { globals } from 'src/environments/environment.prod';
 import { Livro } from './modelos/Livro';
-import { Pedido } from './modelos/Pedido';
-import { PedidoLivro } from './modelos/PedidoLivro';
-import { UsuarioLogin } from './modelos/UsuarioLogin';
-import { CarrinhoService } from './service/carrinho.service';
-import { LocalDate, LocalDateTime } from './utils/LocalDate';
 
 @Component({
   selector: 'app-root',
@@ -15,6 +9,56 @@ import { LocalDate, LocalDateTime } from './utils/LocalDate';
 export class AppComponent {
   title = 'thebookclub-angular';
 
+  livros: Livro[];
+
+  private createLivro(
+    titulo: string,
+    foto: string,
+    autores: string,
+    preco: number
+  ): Livro {
+    const l = new Livro();
+
+    l.titulo = titulo;
+    l.foto = foto;
+    l.autores = autores;
+    l.preco = preco;
+
+    return l;
+  }
+
   constructor() { 
+    const livros = [
+      this.createLivro(
+        "Os segredos da mente milionária",
+        "https://images-na.ssl-images-amazon.com/images/I/81ZnJcgjCdL.jpg",
+        "T. Harv Eker",
+        30.9
+      ),
+      this.createLivro(
+        "As Crônicas de Nárnia",
+        "https://images-na.ssl-images-amazon.com/images/I/71yJLhQekBL.jpg",
+        "C. S. Lewis",
+        89.9
+      ),
+      this.createLivro(
+        "Star Wars: Marcas da Guerra",
+        "https://images-na.ssl-images-amazon.com/images/I/91wNLQh9oPL.jpg",
+        "Chuck Wendig",
+        33.9
+      ),
+      this.createLivro(
+        "Harry Potter e o Prisioneiro de Azkaban",
+        "https://images-na.ssl-images-amazon.com/images/I/81u+ljPVifL.jpg",
+        "J.K. Rowling",
+        35.9
+      ),
+    ];
+
+    this.livros = [];
+
+    for (let i = 0; i < 32; i++) {
+      this.livros.push(livros[i % livros.length]);
+    }
   }
 }
