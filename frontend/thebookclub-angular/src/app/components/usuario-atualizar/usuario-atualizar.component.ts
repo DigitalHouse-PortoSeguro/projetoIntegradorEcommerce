1d83b9ef10c7a4d1b25e71e7e33fd965b7d65a3c
ghsoares <ghsoares99795@gmail.com>
2022-06-21 15:10:47 -0300

    Adicionado atualização de usuário

---
+++ frontend/thebookclub-angular/src/app/components/usuario-atualizar/usuario-atualizar.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from 'src/app/modelos/Usuario';
+import { UsuarioService } from 'src/app/service/usuario.service';
+
+@Component({
+  selector: 'app-usuario-atualizar',
+  templateUrl: './usuario-atualizar.component.html',
+  styleUrls: ['./usuario-atualizar.component.css']
+})
+export class UsuarioAtualizarComponent implements OnInit {
+
+  user: Usuario;
+
+  constructor(
+    private usuarioService: UsuarioService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    if (!this.usuarioService.isLoggedIn()) {
+      this.router.navigate(['login']);
+      return;
+    }
+    this.user = this.usuarioService.getUsuario();
+  }
+
+  atualizar(usuario: Usuario): void {
+    this.usuarioService.atualizarUsuario(usuario).subscribe({
+      next: resp => {
+        console.log(resp);
+        this.usuarioService.logout();
+        this.router.navigate(['login']);
+        alert('Usuário atualizado com sucesso!');
+      },
+      error: err => {
+        console.log(err);
+        alert('Um erro aconteceu...');
+      }
+    })
+  }
+
+}
