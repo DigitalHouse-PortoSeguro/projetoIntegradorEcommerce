bc69d8075688dea541528d7baa5876d4589e41a2
ghsoares <ghsoares99795@gmail.com>
2022-06-21 15:11:14 -0300

    Modificado rotas

--- frontend/thebookclub-angular/src/app/app-routing.module.ts
+++ frontend/thebookclub-angular/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { UsuarioCadastroComponent } from './components/usuario-cadastro/usuario-cadastro.component';
+import { UsuarioAtualizarComponent } from './components/usuario-atualizar/usuario-atualizar.component';
 
 const routes: Routes = [
-  { path:"usuarios/cadastrar", component: UsuarioCadastroComponent }
+  { path: "cadastrar", component: UsuarioCadastroComponent },
+  { path: "atualizar", component: UsuarioAtualizarComponent },
 ]
 
 @NgModule({
