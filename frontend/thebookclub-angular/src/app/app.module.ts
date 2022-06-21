916dcaed36ec9b0134bb305f8466fdf13cfe34c7
ghsoares <ghsoares99795@gmail.com>
2022-06-21 15:15:38 -0300

    Adicionado form de usuário

--- frontend/thebookclub-angular/src/app/app.module.ts
+++ frontend/thebookclub-angular/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { UsuarioCadastroComponent } from './components/usuario-cadastro/usuario-cadastro.component';
+import { FormUsuarioComponent } from './components/form-usuario/form-usuario.component';
 import { InputCustomComponent } from './components/input-custom/input-custom.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -11,16 +11,20 @@ import { DropdownComponent } from './components/dropdown/dropdown.component';
 import { FormDebugComponent } from './components/form-debug/form-debug.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CardLivroComponent } from './components/livro/card-livro/card-livro.component';
+import { UsuarioCadastroComponent } from './components/usuario-cadastro/usuario-cadastro.component';
+import { UsuarioAtualizarComponent } from './components/usuario-atualizar/usuario-atualizar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsuarioCadastroComponent,
+    FormUsuarioComponent,
     InputCustomComponent,
     InputFieldComponent,
     DropdownComponent,
     FormDebugComponent,
-    CardLivroComponent
+    CardLivroComponent,
+    UsuarioCadastroComponent,
+    UsuarioAtualizarComponent
   ],
   imports: [
     BrowserModule,
