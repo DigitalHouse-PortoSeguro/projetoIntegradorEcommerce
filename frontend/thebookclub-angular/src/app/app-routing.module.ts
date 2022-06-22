6a0e49b4a12145204dff4cd1b979d69d33132ea0
ghsoares <ghsoares99795@gmail.com>
2022-06-22 14:39:45 -0300

    Adicionado rotas admin

--- frontend/thebookclub-angular/src/app/app-routing.module.ts
+++ frontend/thebookclub-angular/src/app/app-routing.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { UsuarioCadastroComponent } from './components/usuario-cadastro/usuario-cadastro.component';
 import { UsuarioAtualizarComponent } from './components/usuario-atualizar/usuario-atualizar.component';
+import { ListarUsuariosComponent } from './components/listar-usuarios/listar-usuarios.component';
+import { GerenciarUsuarioComponent } from './components/gerenciar-usuario/gerenciar-usuario.component';
+import { UsuarioAtualizarAdminComponent } from './components/usuario-atualizar-admin/usuario-atualizar-admin.component';
 
 const routes: Routes = [
-  {path: "", redirectTo:"cadastrar", pathMatch:"full"},
+  { path: "", redirectTo: "cadastrar", pathMatch: "full" },
   { path: "cadastrar", component: UsuarioCadastroComponent },
   { path: "atualizar", component: UsuarioAtualizarComponent },
+  { path: "admin/listar-usuarios", component: ListarUsuariosComponent },
+  { path: "admin/gerenciar-usuario/:id", component: GerenciarUsuarioComponent },
+  { path: "admin/atualizar-usuario/:id", component: UsuarioAtualizarAdminComponent },
 ]
 
 @NgModule({
