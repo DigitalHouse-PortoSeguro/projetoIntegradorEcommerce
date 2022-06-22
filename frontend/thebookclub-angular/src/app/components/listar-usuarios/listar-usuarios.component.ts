5abddf92abe679b8fa135dd5d752ff598db8ebf0
ghsoares <ghsoares99795@gmail.com>
2022-06-22 14:41:01 -0300

    Adicionado listagem de usuários como admin

--- frontend/thebookclub-angular/src/app/components/listar-usuarios/listar-usuarios.component.ts
+++ frontend/thebookclub-angular/src/app/components/listar-usuarios/listar-usuarios.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from 'src/app/modelos/Usuario';
+import { UsuarioService } from 'src/app/service/usuario.service';
 
 @Component({
   selector: 'app-listar-usuarios',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarUsuariosComponent implements OnInit {
 
-  constructor() { }
+  usuarios: Usuario[] = [];
+
+  constructor(
+    private usuarioService: UsuarioService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    if (!this.usuarioService.isLoggedIn()) {
+      this.router.navigate(['login']);
+      return;
+    }
+    if (!this.usuarioService.isAdmin()) {
+      this.router.navigate(['inicio']);
+      return;
+    }
+
+    this.usuarioService.getAllUsuarios().subscribe({
+      next: resp => {
+        this.usuarios = resp;
+      },
+      error: err => {
+        alert('Um erro aconteceu...');
+        console.log(err);
+      }
+    });
+  }
+
+  gerenciar(usuarioId: number): void {
+    this.router.navigate(['admin/gerenciar-usuario', usuarioId]);
   }
 
 }
