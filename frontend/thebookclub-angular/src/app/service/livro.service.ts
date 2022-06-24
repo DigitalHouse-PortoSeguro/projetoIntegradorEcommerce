7e2991cee2f30afbcf181e1d8d3b0a562c840541
ghsoares <ghsoares99795@gmail.com>
2022-06-24 16:41:47 -0300

    Adicionado livro service

---
+++ frontend/thebookclub-angular/src/app/service/livro.service.ts
@@ -0,0 +1,47 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { globals } from 'src/environments/environment.prod';
+import { Livro } from '../modelos/Livro';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LivroService {
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  public cadastrarLivro(livro: Livro): Observable<Livro> {
+    const headers = new HttpHeaders()
+      .set('Authorization', globals.usuarioLogin.token);
+    
+    return this.http.post<Livro>('http://localhost:8080/livros/cadastrar', livro, { headers });
+  }
+
+  public atualizarLivro(livro: Livro): Observable<Livro> {
+    const headers = new HttpHeaders()
+      .set('Authorization', globals.usuarioLogin.token);
+    
+    return this.http.put<Livro>('http://localhost:8080/livros/atualizar', livro, { headers });
+  }
+
+  public deletarLivro(id: number): Observable<Livro> {
+    const headers = new HttpHeaders()
+      .set('Authorization', globals.usuarioLogin.token);
+    
+    return this.http.delete<Livro>(`http://localhost:8080/livros/${id}`, { headers });
+  }
+
+  public getAllLivros(): Observable<Livro[]> {
+    return this.http.get<Livro[]>(`http://localhost:8080/livros`);
+  }
+
+  public getLivroById(id: number): Observable<Livro> {
+    return this.http.get<Livro>(`http://localhost:8080/livros/${id}`);
+  }
+
+  public getLivroByTitulo(id: number): Observable<Livro> {
+    return this.http.get<Livro>(`http://localhost:8080/livros/titulo/${id}`);
+  }
+}
