4592991b1342ed96e556446d3b7fc9dacd41745b
Adriana Mucciolo <drishaolin@gmail.com>
2022-06-28 17:37:17 -0300

    adicionada rota para form cadastro de livros

--- frontend/thebookclub-angular/src/app/app-routing.module.ts
+++ frontend/thebookclub-angular/src/app/app-routing.module.ts
@@ -10,15 +10,17 @@ import { AppComponent } from './app.component';
 import { LivrosTituloComponent } from './components/livro/livros-titulo/livros-titulo.component';
 import { LivroCategoriaComponent } from './components/livro/livro-categoria/livro-categoria.component';
 import { ProdutoLivroComponent } from './components/livro/produto-livro/produto-livro.component';
+import { LivroCadastroComponent } from './components/livro/livro-cadastro/livro-cadastro.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "cadastrar", pathMatch: "full" },
-  { path: "cadastrar", component: UsuarioCadastroComponent },
+  { path: "cadastrar", component: UsuarioCadastroComponent },//arrumar para usuarios/cadastrar
   { path: "atualizar", component: UsuarioAtualizarComponent },
   { path: "login", component: AppComponent },
   { path: "admin/listar-usuarios", component: ListarUsuariosComponent },
   { path: "admin/gerenciar-usuario/:id", component: GerenciarUsuarioComponent },
   { path: "admin/atualizar-usuario/:id", component: UsuarioAtualizarAdminComponent },
+  { path: "livros/cadastrar", component: LivroCadastroComponent },
   { path: "livros/titulo/:titulo", component: LivrosTituloComponent },
   { path: "livros/categoria/:categoria", component: LivroCategoriaComponent},
   { path: "livros/produto/:id", component: ProdutoLivroComponent }
