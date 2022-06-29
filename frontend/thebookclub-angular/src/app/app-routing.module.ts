5b158e3d08936bf58bc21c9d721462974636087f
Rui Temgi <ruisaraivasp@gmail.com>
2022-06-29 17:06:25 -0300

    Refatoração formulário de cadastro livro

--- frontend/thebookclub-angular/src/app/app-routing.module.ts
+++ frontend/thebookclub-angular/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { UsuarioCadastroComponent } from './components/usuario-cadastro/usuario-cadastro.component';
 import { UsuarioAtualizarComponent } from './components/usuario-atualizar/usuario-atualizar.component';
+import { LivroCadastroComponent } from './components/livro/livro-cadastro/livro-cadastro.component';
 
 const routes: Routes = [
   {path: "", redirectTo:"cadastrar", pathMatch:"full"},
   { path: "cadastrar", component: UsuarioCadastroComponent },
   { path: "atualizar", component: UsuarioAtualizarComponent },
+  { path: "admin/cadastrarLivro", component: LivroCadastroComponent },
 ]
 
 @NgModule({
--- frontend/thebookclub-angular/src/app/components/livro/livro-cadastro/livro-cadastro.component.ts
+++ frontend/thebookclub-angular/src/app/components/livro/livro-cadastro/livro-cadastro.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup} from '@angular/forms';
+import { Livro } from 'src/app/modelos/Livro';
+import { LivroService } from 'src/app/service/livro.service';
+import { LocalDate } from 'src/app/utils/LocalDate';
 
 @Component({
   selector: 'app-livro-cadastro',
@@ -7,9 +11,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LivroCadastroComponent implements OnInit {
 
-  constructor() { }
+  form : FormGroup
 
-  ngOnInit(): void {
+  constructor(private FormBuilder : FormBuilder,
+              private LivroService : LivroService) { }
+
+  ngOnInit(){
+    this.form = this.FormBuilder.group({
+      Titulo : ['', ],
+      DataPublicacao : ['', ],
+      Autores : ['', ],
+      Editora : ['', ],
+      Categoria : ['', ],
+      NumeroDePaginas : ['', ],
+      Preco : ['', ],
+      ISBN : ['', ],
+      QuantidadeNoEstoque : ['', ],
+      Fornecedor : ['', ]
+    })
   }
+
+  enviar(){
+    if(!this.form.valid){
+      alert('Formulário está Incompleto!!!')
+      return
+    }
+    console.log(this.form.get('DataPublicacao')!.value)
+    let livro = new Livro()
+    
+    livro.titulo = this.form.get('Titulo')!.value
+    livro.dataPublicacao = LocalDate.fromString(this.form.get('Titulo')!.value) 
+    livro.autores = this.form.get('Autores')!.value
+    livro.editora = this.form.get('Editora')!.value
+    livro.categoria = this.form.get('Categoria')!.value
+    livro.numeroPaginas = Number(this.form.get('NumeroDePaginas')!.value)
+    livro.preco = Number(this.form.get('Preco')!.value)
+    livro.isbn = this.form.get('ISBN')!.value
+    livro.quantidadeEstoque = Number(this.form.get('QuantidadeNoEstoque')!.value)
+    livro.fornecedor = this.form.get('Fornecedor')!.value
+
+    this.LivroService.cadastrarLivro(livro).subscribe({
+      next : LivroResposta => {alert('Livro Cadastrado!!!')},
+      error : Erro => {alert('Erro ao Cadastrar o Livro!!!')
+              console.log(Erro)  
+    }
+    })
+  }
+  
+
 
 }
