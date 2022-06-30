57e75aec5c56ecb69575aadd4369f0ad50c5feca
ghsoares <ghsoares99795@gmail.com>
2022-06-30 17:28:13 -0300

    Alterações na página admin

--- frontend/thebookclub-angular/src/app/components/admin/admin.component.ts
+++ frontend/thebookclub-angular/src/app/components/admin/admin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UsuarioService } from 'src/app/service/usuario.service';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(private usuarioService: UsuarioService, private router: Router) { }
 
   ngOnInit(): void {
+    if (!this.usuarioService.isLoggedIn()) {
+      this.router.navigate(['entrar']);
+      return;
+    }
+    if (this.usuarioService.isLoggedIn() && !this.usuarioService.isAdmin()) {
+      this.router.navigate(['inicio']);
+      return;
+    }
   }
 
 }
