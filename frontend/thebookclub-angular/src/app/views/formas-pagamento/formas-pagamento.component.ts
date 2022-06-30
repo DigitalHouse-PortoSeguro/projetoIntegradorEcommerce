52b7e33de8a3466ff114d14037d0b081397169ae
Adrielle Barros <adriellebarros31@gmail.com>
2022-06-29 22:43:26 -0300

    Criação de paginas fale conosco, formas de pagamento e sobre nos, disponiveis no rodape do site

---
+++ frontend/thebookclub-angular/src/app/views/formas-pagamento/formas-pagamento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formas-pagamento',
+  templateUrl: './formas-pagamento.component.html',
+  styleUrls: ['./formas-pagamento.component.css']
+})
+export class FormasPagamentoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
