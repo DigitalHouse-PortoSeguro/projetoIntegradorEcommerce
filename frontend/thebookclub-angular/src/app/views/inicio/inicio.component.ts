b8fdc0a1ef758e05c1aeb89e7700a69316b41f0f
ghsoares <ghsoares99795@gmail.com>
2022-06-30 23:31:47 -0300

    Modificado inicio

--- frontend/thebookclub-angular/src/app/views/inicio/inicio.component.ts
+++ frontend/thebookclub-angular/src/app/views/inicio/inicio.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Livro } from 'src/app/modelos/Livro';
+import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
   selector: 'app-inicio',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
+  listaLivros: Livro[];
+
+  constructor(
+    private livroService: LivroService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.livroService.getAllLivros().subscribe({
+      next: livros => {
+        for (let i = livros.length - 1; i > 0; i--) {
+          let j = Math.floor(Math.random() * livros.length);
+
+          let temp = livros[j];
+          livros[j] = livros[i];
+          livros[i] = temp;
+        }
+        this.listaLivros = livros.slice(0, 10);
+      },
+      error: err => {
+        alert("Um erro aconteceu");
+        console.log(err);
+      }
+    })
+  }
+
+  onLivroSelect(id: number): void {
+    console.log(id);
+    this.router.navigate(['/livros/produto', id]);
   }
 
 }
